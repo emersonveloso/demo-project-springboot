9579c6da2d80f891fd3d0526559a864a6bb65e1e
Emerson Veloso <emesonveloso.s@gmail.com>
2023-01-04 19:29:48 -0300

    Project created

---
+++ src/main/java/br/com/demo/demoprojectspringboot/DemoprojectspringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.demo.demoprojectspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoprojectspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoprojectspringbootApplication.class, args);
+	}
+
+}
